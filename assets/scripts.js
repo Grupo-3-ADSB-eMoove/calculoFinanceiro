f8bcb30534a5697828d814524d997d7c589f033a
matheuslima19 <matheus.slima@sptech.school>
2023-03-13 15:17:56 -0300

    Segundo commit do calculo financeiro com a exibição bugada.

--- assets/scripts.js
+++ assets/scripts.js
@@ -23,22 +23,22 @@ function calcularGanho(){
     
     var resposta = document.getElementById('resposta')
     
-   if(qtdVendas > entradasTotais){
+  if(qtdVendas > entradasTotais){
     alert('Suas vendas não podem sem maiores que suas entradas')
-   }else{
+  }else{
 if(taxaConversao < 30){
 exibeCalculo.innerHTML = `<span class="spanRed">Com esses números oferecidos sua taxa de conversão é: <b>${taxaConversao}%</b></span> <br><br>
-Atualmente seu estabelecimento faz ${qtdVendas} mensais <br><br>
+Atualmente seu estabelecimento faz <b>${qtdVendas}</b> mensais <br><br>
 Aplicando nossa solução e utilizando nossos serviços de maneira correta suas vendas podem aumentar em até <b>60%</b>
-E nessa hipótese seu valor total de vendas mensais seria de ${(qtdVendasPosConversao).toFixed}
-e se sua renda acompanhasse seria de R$:${(qtdVendasPosConversao *mediaPorComprador).toFixed(0)}<br><br>`
+E nessa hipótese seu valor total de vendas mensais seria de <b>${(qtdVendasPosConversao).toFixed}</b>
+e se sua renda acompanhasse seria de <b>R$:${(qtdVendasPosConversao *mediaPorComprador).toFixed(0)}</b><br><br>`
 }
   if(taxaConversao >= 30) {
     exibeCalculo.innerHTML = `<span class="spanTexto">Com esses números oferecidos sua taxa de conversão é: <b>${taxaConversao}%</b></span> <br><br>
-Atualmente seu estabelecimento faz ${qtdVendas} mensais <br><br>
+Atualmente seu estabelecimento faz <b>${qtdVendas}</b> mensais <br><br>
 Aplicando nossa solução e utilizando nossos serviços de maneira correta suas vendas podem aumentar em até <b>60%</b>
-E nessa hipótese seu valor total de vendas mensais seria de ${(qtdVendasPosConversao).toFixed(0)} e se sua renda acompanhasse seria 
-de R$:${(qtdVendasPosConversao *mediaPorComprador).toFixed(0)}<br><br>`
+E nessa hipótese seu valor total de vendas mensais seria de <b>${(qtdVendasPosConversao).toFixed(0)}</b> e se sua renda acompanhasse seria 
+de <b>R$:${(qtdVendasPosConversao *mediaPorComprador).toFixed(0)}</b><br><br>`
   } 
 
 
@@ -46,11 +46,11 @@ de R$:${(qtdVendasPosConversao *mediaPorComprador).toFixed(0)}<br><br>`
 
     if(inpSemProblemas.checked) {
         exibeCalculo.innerHTML += `É bom que seu estabelecimento não tenha problemas com filas mas ainda podemos melhorar sua conversão`
-   }
-   if(inpPoucosProblemas.checked){
+  }
+  if(inpPoucosProblemas.checked){
         exibeCalculo.innerHTML += `Segundo pesquisas depois de 2 minutos em filas, seus clientes começarão a ficar frustrados e você está começando a perde-los`
-   }
-   if(inpAlgunsProblemas.checked){
+  }
+  if(inpAlgunsProblemas.checked){
 
     // preço_com_desconto_percentual_incluído / (1- taxa_de_desconto) / 100)
     // inverter desconto
@@ -61,28 +61,27 @@ de R$:${(qtdVendasPosConversao *mediaPorComprador).toFixed(0)}<br><br>`
 
     exibeCalculo.innerHTML += `Segundo a "Retail Costumer Experience" 1/3 
     dos clientes que esperam até 5 minutos nas filas abandonam a mesma logo em seguida.<br><br>
-                                                         
     Portanto, é possível dizer que seu estabelecimento deixou de fazer 1/3 das vendas. Caso não 
-    houvesse problemas com as filas seu total de vendas seria de  ${qtdVendasPosFilas.toFixed(0)} 
-    vendas e você teria arrecadado R$:${(qtdVendasPosFilas*mediaPorComprador).toFixed(0)}<br><br>
-    E se combinassemos tudo seu estabelecimento teria <b>${qtdVendasCombinando.toFixed(0)}</b> vendas mensais e uma renda de 
+    houvesse problemas com as filas seu total de vendas seria de aproximadamente <b>${qtdVendasPosFilas.toFixed(0)}</b> 
+    vendas e você teria arrecadado aproximadamente <b>R$:${(qtdVendasPosFilas*mediaPorComprador).toFixed(0)}</b><br><br>
+    E se combinassemos tudo seu estabelecimento teria aproximadamente <b>${qtdVendasCombinando.toFixed(0)}</b> vendas mensais e uma renda de aproximadamente
     <b>R$:${(qtdVendasCombinando *mediaPorComprador).toFixed(0)}</b> 
     `
-   }
+  }
 
-   if(inpMuitosProblemas.checked){
+  if(inpMuitosProblemas.checked){
 
     qtdVendasCombinando = qtdVendasCombinando /(1-(50/100))
 
     qtdVendasPosFilas = qtdVendas/(1-(50/100))
     exibeCalculo.innerHTML += `50% dos seus clientes vão desistir de comprar se esperarem mais do que cinco minutos na fila segundo pesquisas<br><br>
     Então é possível que seus problemas com filas estejam diminuindo pela metade as suas vendas 
-    e se não houvessem problemas com filas seu total de vendas seria de ${qtdVendasPosFilas.toFixed(0)} 
-    e você teria arrecadado R$:${(qtdVendasPosFilas*mediaPorComprador).toFixed(0)}<br><br>
-    E se combinassemos tudo seu estabelecimento teria <b>${qtdVendasCombinando}</b> vendas mensais e uma renda de 
+    e se não houvessem problemas com filas seu total de vendas seria de <b>${qtdVendasPosFilas.toFixed(0)}</b> 
+    e você teria arrecadado <b>R$:${(qtdVendasPosFilas*mediaPorComprador).toFixed(0)}</b><br><br>
+    E se combinassemos tudo seu estabelecimento teria <b>${qtdVendasCombinando}</b> vendas mensais e uma renda de aproximadamente
     <b>R$:${(qtdVendasCombinando *mediaPorComprador).toFixed(0)}</b> 
     `
-   }
+  }
     
 }
 
