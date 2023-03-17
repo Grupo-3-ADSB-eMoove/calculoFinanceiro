92c889e8b49ce5868b9db40256331661e017cf93
matheusslima19 <matheus.slima@sptech.school>
2023-03-16 21:41:33 -0300

    Commit final da sprint 1 !!!

--- tabelaEmoove.sql
+++ tabelaEmoove.sql
@@ -32,10 +32,10 @@ idCaixa INT PRIMARY KEY NOT NULL auto_increment,
 qtdCaixa INT not null
 );
 
-INSERT INTO Usuario VALUES 
-(null, 'Matheus Lima', '11943098276' ,'#GF123456789', 'pago');
+INSERT INTO Usuario (idCliente, nome, telefone, senha, situacaoCliente)VALUES 
+(null, 'Matheus Lima', '11943098276' ,'#GF123456789', 'invalido');
 
-INSERT INTO Estabelecimento VALUES 
+INSERT INTO Estabelecimento (idCNPJ, nomeFantasia, endereco, cep)VALUES 
 ('06818595000173', 'Farmácia XPTO', 'Avenida Paulista', '01311000');
 
 INSERT INTO Compra VALUES 
@@ -54,4 +54,22 @@ SELECT * FROM Compra;
 SELECT * FROM sensorEntrada;
 SELECT * FROM sensorCaixa;
 
+ALTER TABLE Usuario ADD constraint chkSituacao CHECK (situacaoCliente = 'pago' or situacaoCliente = 'invalido');
 
+ALTER TABLE Usuario ADD COLUMN fkCNPJ char(14),
+	ADD foreign key (fkCNPJ) REFERENCES Estabelecimento(idCNPJ);
+
+ALTER TABLE Usuario ADD COLUMN fkCompra int,
+	ADD foreign key (fkCompra) REFERENCES Compra(idCompra);
+
+ALTER TABLE Estabelecimento ADD COLUMN fkSensorEntrada int,
+	ADD foreign key (fkSensorEntrada) REFERENCES sensorEntrada(idEntrada);
+
+ALTER TABLE Estabelecimento ADD COLUMN fkSensorCaixa int,
+	ADD foreign key (fkSensorCaixa) REFERENCES sensorCaixa(idCaixa);
+
+UPDATE Usuario set fkCNPJ = '06818595000173', fkCompra = '1'
+	WHERE idCliente = 1;
+    
+UPDATE Estabelecimento set fkSensorEntrada = '1', fkSensorCaixa = '1'
+	WHERE idCNPJ = '06818595000173';
